023a5e577c34ea7495812ea2c57e8c506c3f9dbf
John Hortua <1109660285@u.icesi.edu.co>
2023-05-03 12:25:26 -0500

    Added lists for Product and Order

--- src/main/java/org/integradora2/model/MercadoLibre.java
+++ src/main/java/org/integradora2/model/MercadoLibre.java
@@ -1,4 +1,15 @@
 package org.integradora2.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MercadoLibre {
+    private List<Product> products;
+    private List<Order> orders;
+
+    public MercadoLibre() {
+        products = new LinkedList<>();
+        orders = new LinkedList<>();
+    }
+
 }
