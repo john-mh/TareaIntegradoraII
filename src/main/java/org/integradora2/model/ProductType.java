d1a2fcf66aff35c32e766dd362a35bddd80c6da6
John Hortua <1109660285@u.icesi.edu.co>
2023-05-02 21:53:05 -0500

    Added the types of Product

--- src/main/java/org/integradora2/model/ProductType.java
+++ src/main/java/org/integradora2/model/ProductType.java
@@ -1,4 +1,16 @@
 package org.integradora2.model;
 
 public enum ProductType {
+
+    BOOKS("Books"), ELECTRONICS("Electronics"), CLOTHES_AND_ACCESSORIES("Clothes & Accessories"), FOOD_AND_BEVERAGES("Food & Beverages"), SPORTS("Sports"), TOYS_AND_GAMES("Toys & Games"), BEAUTY_AND_PERSONAL_CARE("Beauty & Personal Care"), STATIONERY("Stationery");
+
+    private String category;
+
+    ProductType(String category) {
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
 }
