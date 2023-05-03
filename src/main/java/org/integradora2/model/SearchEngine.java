6dbab3f6f587ba5c7d8297d1c84281899f01c52a
John Hortua <1109660285@u.icesi.edu.co>
2023-05-03 12:07:40 -0500

    Added the SearchEngine class. This class will work as an intermediary between Searcher and MercadoLibre

--- src/main/java/org/integradora2/model/SearchEngine.java
+++ src/main/java/org/integradora2/model/SearchEngine.java
@@ -1,4 +1,24 @@
 package org.integradora2.model;
 
-public class SearchEngine<T extends Searchable<T>> {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.function.Function;
+
+public class SearchEngine {
+
+    public <T extends Searchable<T>> List<T> searchingString(List<T> items, String query, Function<T, String> fieldGetter) {
+        return Searcher.searchingString(items, query, fieldGetter);
+    }
+
+    public <T extends Searchable<T>> List<T> searchingInt(List<T> items, int value, Function<T, Integer> fieldGetter) {
+        return Searcher.searchingInt(items, value, fieldGetter);
+    }
+
+    public <T extends Searchable<T>> List<T> searchingDouble(List<T> items, double value, Function<T, Double> fieldGetter) {
+        return Searcher.searchingDouble(items, value, fieldGetter);
+    }
+
+    public <T extends Searchable<T>> List<T> searchingDate(List<T> orders, LocalDate value, Function<T, LocalDate> fieldGetter) {
+        return Searcher.searchingDate(orders, value, fieldGetter);
+    }
 }
