2f8dc85efac72077a3d99353df1065a472098cfd
John Hortua <1109660285@u.icesi.edu.co>
2023-05-04 10:34:02 -0500

    Changed the generic parameter of the list, now it can only receive a list of Product instead of anything Searchable

--- src/main/java/org/integradora2/model/Order.java
+++ src/main/java/org/integradora2/model/Order.java
@@ -5,4 +5,4 @@ import org.jetbrains.annotations.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
-public record Order(List<? extends Searchable<?>> products, LocalDate date, String name, double totalPrice) implements Searchable<Order> {}
+public record Order(List<Product> products, LocalDate date, String name, double totalPrice) implements Searchable<Order> {}
