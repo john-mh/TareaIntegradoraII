20e94049fce8aaebda8c74c6377d28d5285b26ef
John Hortua <1109660285@u.icesi.edu.co>
2023-05-04 10:10:54 -0500

    Imported the LocalDate library. Added the SearchEngine attribute. Made all attributes final. Added getters for the lists. Added the add and delete method por Product and Order, added the increase of times bought and product quantity, and the decrease of product quantity.

--- src/main/java/org/integradora2/model/MercadoLibre.java
+++ src/main/java/org/integradora2/model/MercadoLibre.java
@@ -1,15 +1,59 @@
 package org.integradora2.model;
 
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MercadoLibre {
-    private List<Product> products;
-    private List<Order> orders;
+    private final List<Product> products;
+    private final List<Order> orders;
+    private final SearchEngine searchEngine;
 
     public MercadoLibre() {
         products = new LinkedList<>();
         orders = new LinkedList<>();
+        searchEngine = new SearchEngine();
     }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void addProduct(String name, String description, double price, int timesBought, int availableQuantity, int type) {
+        products.add(new Product(name, description, price, timesBought, availableQuantity, type));
+    }
+
+    public void addOrder(List<Integer> productsIndexes, LocalDate date, String name, double totalPrice) {
+        for(Integer i : productsIndexes) {
+            increaseTimesBought(i, 1);
+            decreaseProductQuantity(i, 1);
+        }
+        orders.add(new Order(searchEngine.collectMatches(this.products, productsIndexes), date, name, totalPrice));
+    }
+
+    public void deleteProduct(int index) {
+        products.remove(index);
+    }
+
+    public void deleteOrder(int index) {
+        orders.remove(index);
+    }
+
+    public void increaseTimesBought(int index, int quantity) {
+        products.get(index).setTimesBought(products.get(index).getTimesBought() + quantity);
+    }
+
+    public void increaseProductQuantity(int index, int quantity) {
+        products.get(index).setAvailableQuantity(products.get(index).getAvailableQuantity() + quantity);
+    }
+
+    public void decreaseProductQuantity(int index, int quantity) {
+        products.get(index).setAvailableQuantity(products.get(index).getAvailableQuantity() - quantity);
+    }
+
 
 }
