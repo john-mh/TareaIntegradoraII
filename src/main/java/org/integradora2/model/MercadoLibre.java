d8ac20187a50a687b62199d51b210d6985b1c4e9
John Hortua <1109660285@u.icesi.edu.co>
2023-05-04 10:27:41 -0500

    Added the search methods using SearchEngine

--- src/main/java/org/integradora2/model/MercadoLibre.java
+++ src/main/java/org/integradora2/model/MercadoLibre.java
@@ -32,7 +32,7 @@ public class MercadoLibre {
             increaseTimesBought(i, 1);
             decreaseProductQuantity(i, 1);
         }
-        orders.add(new Order(searchEngine.collectMatches(this.products, productsIndexes), date, name, totalPrice));
+        orders.add(new Order(searchEngine.collectMatches(products, productsIndexes), date, name, totalPrice));
     }
 
     public void deleteProduct(int index) {
@@ -55,5 +55,36 @@ public class MercadoLibre {
         products.get(index).setAvailableQuantity(products.get(index).getAvailableQuantity() - quantity);
     }
 
+    public List<Product> searchProductByName(String query) {
+        return searchEngine.searchingString(products, query, Product::getName);
+    }
+
+    public List<Product> searchProductByCategory(String query) {
+        return searchEngine.searchingString(products, query, Product::getCategory);
+    }
+
+    public List<Product> searchProductByPrice(double query) {
+        return searchEngine.searchingDouble(products, query, Product::getPrice);
+    }
+
+    public List<Product> searchProductByTimesBought(int query) {
+        return searchEngine.searchingInt(products, query, Product::getTimesBought);
+    }
+
+    public List<Product> searchProductByRangePrice(double min, double max) {
+        return searchEngine.searchingByRange(products, min, max, Product::getPrice);
+    }
+
+    public List<Order> searchOrderByName(String query) {
+        return searchEngine.searchingString(orders, query, Order::name);
+    }
+
+    public List<Order> searchOrderByTotalPrice(double query) {
+        return searchEngine.searchingDouble(orders, query, Order::totalPrice);
+    }
+
+    public List<Order> searchOrderByDate(LocalDate query) {
+        return searchEngine.searchingDate(orders, query, Order::date);
+    }
 
 }
